61ed309567adf75e0303b00cd821c0b60d209cb4
Nomthandazo Duda <ALATHA@users.noreply.github.com>
2015-09-29 09:45:48 +0200

    Update task3.js

--- task3.js
+++ task3.js
@@ -3,6 +3,7 @@ var redbutton= document.getElementById("redbutton");
 redbutton.addEventListener("click", function(){
 var block =document.getElementById("block");
 block.className="red";
+
 lock=document.getElementById("block");
 document.getElementById("block");
 block.innerHTML="stop";
